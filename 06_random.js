9c53106a4609c363d3fee7f3c1451c7fa2803808
Szabolcs Juhász <183376403+juhasz-szabolcs@users.noreply.github.com>
2026-01-15 11:58:54 +0100

    random kiíratása

--- 06_random.js
+++ 06_random.js
@@ -2,6 +2,9 @@ console.log(Math.random());
 // console.log(Math.random()*10);
 console.log(Math.floor(Math.random()*100))
 console.log(Math.floor(Math.random()*100)+100)
-let dobas = (Math.floor(Math.random()*100)+400)
-// console.log(Math.floor(Math.random()*1000)+100)
-document.getElementById("dobas").innerText = dobas;
+function dobas_mutat() {
+    let dobas = (Math.floor(Math.random()*100)+400)
+    // console.log(Math.floor(Math.random()*1000)+100)
+    document.getElementById("dobas").innerText = dobas;
+
+}
